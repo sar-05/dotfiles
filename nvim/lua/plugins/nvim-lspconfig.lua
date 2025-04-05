476da6278e0e99ff04dac75c8ae04d49b2d0d115
SAR <sebastian.alighierir@uanl.edu.mx>
2025-04-05 16:55:29 -0600

    Fixed LSP server not loading for .py files. Added keybindings for LSP functionalities

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -32,6 +32,7 @@ return {
         'lemminx',
         'marksman',
         'quick_lint_js',
+        'pyright',
         -- 'tsserver', -- requires npm to be installed
         -- 'yamlls', -- requires npm to be installed
       }
@@ -41,6 +42,21 @@ return {
     local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
     local lsp_attach = function(client, bufnr)
       -- Create your keybindings here...
+      -- Keybindings for LSP functionality
+      local opts = { noremap=true, silent=true, buffer=bufnr }
+  
+      -- Navigation
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  
+      -- Code actions and diagnostics
+     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  
+     -- Print notification when LSP attaches
+     vim.notify("LSP " .. client.name .. " started for buffer " .. bufnr, vim.log.levels.INFO)
     end
 
     -- Call setup on each LSP server
@@ -64,6 +80,59 @@ return {
         },
       },
     }
+    -- Python LSP settings
+    lspconfig.pyright.setup {
+      filetypes = { "python" },
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            diagnosticMode = "workspace",
+            useLibraryCodeForTypes = true
+          },
+        },
+      },
+      on_attach = lsp_attach,
+      capabilities = lsp_capabilities,
+    }
+-- TeX LSP settings
+lspconfig.texlab.setup {
+  settings = {
+    texlab = {
+      -- Disable TexLab's build functionality since we'll use vimtex for this
+      build = {
+        onSave = false,
+        executable = "latexmk",
+        args = {"-pdf", "-interaction=nonstopmode", "-synctex=1", "%f"},
+      },
+      -- Disable TexLab's built-in viewer functionality since we'll use vimtex
+      forwardSearch = {
+        executable = nil,
+        args = {},
+      },
+      chktex = {
+        onOpenAndSave = true,  -- Enable linting on open and save
+        onEdit = false,        -- Disable linting while editing (can be distracting)
+      },
+      diagnosticsDelay = 300,  -- Delay before showing diagnostics (in milliseconds)
+      latexFormatter = "latexindent",  -- Code formatting tool
+      latexindent = {
+        ["local"] = nil,  -- Path to local latexindent config file
+        modifyLineBreaks = false,
+      },
+    },
+  },
+  on_attach = function(client, bufnr)
+    -- Call the existing lsp_attach function first
+    lsp_attach(client, bufnr)
+    
+    -- Apply TeX-specific keymappings here if needed
+    local opts = { noremap=true, silent=true, buffer=bufnr }
+    -- For example:
+    -- vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, opts)
+  end,
+  capabilities = lsp_capabilities,
+}
 
     -- Globally configure all LSP floating preview popups (like hover, signature help, etc)
     local open_floating_preview = vim.lsp.util.open_floating_preview
