dd7defd16cbac6c3e8667c1ecb9763bf748d0f31
SAR <sebastian.alighierir@uanl.edu.mx>
2025-04-22 03:51:15 -0500

    Minor changes to startup plugin

--- nvim/.config/nvim/lua/plugins/startup.lua
+++ nvim/.config/nvim/lua/plugins/startup.lua
@@ -1,6 +1,10 @@
 return{
   "startup-nvim/startup.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
+  dependencies = { 
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim"
+  },
   config = function()
     require "startup".setup()
   end
