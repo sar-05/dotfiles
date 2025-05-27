d46349c268cb69e8a122ab45e9ab69cf7a0e0f6e
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-27 10:23:07 -0600

    Added texlab lsp with config and the ensure_installed at setup

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -7,18 +7,46 @@ return {
     },
     config = function()
       require("mason").setup()
-      require("mason-lspconfig").setup({})
-      opts = {
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          'lua_ls', 'hyprls', 'pyright', 'bashls','marksman', 'taplo', 'texlab'
+        }
+      })
+      Opts = {
         servers = {
           lua_ls = {},
           pyright = {},
           marksman = {},
           bashls = {},
           taplo = {},
-          hyprls = {}
+          hyprls = {},
+          texlab = {
+            -- Disable TexLab's lsp functionality since we'll use vimtex
+            build = {
+              onSave = false,
+              executable = "latexmk",
+              args = {"-pdf", "-interaction=nonstopmode", "-synctex=1", "%f"},
+            },
+            -- Disable TexLab's built-in viewer since we'll use vimtex
+            forwardSearch = {
+              executable = nil,
+              args = {},
+            },
+            chktex = {
+              onOpenAndSave = true,  -- Enable linting on open and save
+              onEdit = false,        -- Disable linting while editing (can be distracting)
+            },
+            diagnosticsDelay = 300,  -- Delay before showing diagnostics (in milliseconds)
+            latexFormatter = "latexindent",  -- Code formatting tool
+            latexindent = {
+              ["local"] = nil,  -- Path to local latexindent config file
+              modifyLineBreaks = false,
+            },
+          }
         }
       }
-      for server, config in pairs(opts.servers) do
+      -- Loop iterates through server-config pairs and enables them
+      for server, config in pairs(Opts.servers) do
         vim.lsp.config(server, config)
         vim.lsp.enable(server)
       end
