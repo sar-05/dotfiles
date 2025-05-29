6455a0c4dedeff3aa10d5fe2d7d05597c653ee14
SAR <sebastian.alighierir@uanl.edu.mx>
2025-05-29 04:36:56 -0600

    Removed bigfile.lua in favor of snacks implementation

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,20 +3,18 @@ return{
   priority = 1000,
   lazy = false,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    bufdelete = { enabled = true },  -- Essential for yazi.nvim
+    picker = { enabled = true },     -- Optional: for Ctrl+S search in yazi
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
-    explorer = { enabled = true },
     indent = { enabled = true },
-    input = { enabled = true },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = false },
+    input = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled = false },
+    scope = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
   },
 }
