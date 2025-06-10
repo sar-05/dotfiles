9d43614bb77db4886a596879acd85e1d56bf2f8f
SAR <sebastian.alighierir@uanl.edu.mx>
2025-06-10 05:09:47 -0500

    Changed default borders to rounded

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,40 +1,40 @@
 -- Session Management
-vim.opt.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 -- Line Numbers
+vim.opt.number         = true
 vim.opt.relativenumber = true
-vim.opt.number = true
 
 -- Tabs & Indentation
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.bo.softtabstop = 2
+vim.opt.tabstop        = 2
+vim.opt.shiftwidth     = 2
+vim.opt.expandtab      = true
+vim.opt.autoindent     = true
+vim.bo.softtabstop     = 2
 
 -- Line Wrapping
-vim.opt.wrap = false
+vim.opt.wrap           = false
 
 -- Search Settings
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.opt.ignorecase     = true
+vim.opt.smartcase      = true
 
 -- Cursor Line
-vim.opt.cursorline = true
+vim.opt.cursorline     = true
 
 -- Appearance
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.opt.signcolumn = "yes"
-vim.opt.showmode = false
-vim.opt.winborder  = "rounded"
+vim.opt.termguicolors  = true
+vim.opt.background     = "dark"
+vim.opt.signcolumn     = "yes"
+vim.opt.showmode       = false
+vim.opt.winborder      = "rounded"
 
 -- Backspace
-vim.opt.backspace = "indent,eol,start"
+vim.opt.backspace      = "indent,eol,start"
 
 -- Split Windows
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.splitright     = true
+vim.opt.splitbelow     = true
 
 -- Consider - as part of keyword
 vim.opt.iskeyword:append("-")
