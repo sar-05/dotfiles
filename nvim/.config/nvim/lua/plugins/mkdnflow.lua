9a10a3f2b873262f02b0f84807e40c1c7121ff6e
sar <sebastian.alighierir@uanl.edu.mx>
2025-06-30 01:44:55 -0500

    Removed date from mkdnflow pattern of new md file

--- nvim/.config/nvim/lua/plugins/mkdnflow.lua
+++ nvim/.config/nvim/lua/plugins/mkdnflow.lua
@@ -5,6 +5,13 @@ return {
       perspective = { --Allows to set an index.md and vimwiki like behaviour
         priority = 'root',
         root_tell = 'index.md'
+      },
+      links = {
+        transform_explicit = function(text)
+          text = text:gsub(" ", "-")
+          text = text:lower()
+          return (text)
+        end
       }
     })
   end
