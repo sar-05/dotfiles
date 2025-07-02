ae2d6314b030ac3593e4d511143ff61a42347617
sar <sebastian.alighierir@uanl.edu.mx>
2025-07-02 02:04:38 -0500

    Updated yazi.lua config to the new default snacks.nvim is no longer a dependency, as yazi.nvim now comes with a copy of the buffdelete plugin embeded

--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -1,32 +1,42 @@
 -- ---@type LazySpec
 return {
-	"mikavilpas/yazi.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		-- check the installation instructions at
-		-- https://github.com/folke/snacks.nvim
-		"folke/snacks.nvim"
-	},
-	keys = {
-		{ "<leader>-",  "<cmd>Yazi<cr>",        desc = "Open yazi at the current file" },
-		{ "<leader>cw", "<cmd>Yazi cwd<cr>",    desc = "Open yazi in nvim's working directory" },
-		{ "<c-up>",     "<cmd>Yazi toggle<cr>", desc = "Resume the last yazi session" }
-	},
-	-- ---@type YaziConfig | {}
-	opts = {
-		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = false,
-		keymaps = {
-			show_help = "<f1>",
-		},
-	},
-	-- 👇 if you use `open_for_directories=true`, this is recommended
-	init = function()
-		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-		-- vim.g.loaded_netrw = 1
-		-- Breaks ability to download spell files!
-		-- https://www.reddit.com/r/neovim/comments/108hnsb/getting_error_in_neovim_warning_cannot_find_word/
-		-- Turn on and off to download a spell file
-		vim.g.loaded_netrwPlugin = 1
-	end,
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  --  ---@type YaziConfig | {}
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    -- vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1 -- Disable to install a new spell file
+  end,
 }
