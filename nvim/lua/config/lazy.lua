1fd39a768edffffe4d050524aa089e9b8f1967ff
sar <sebastian.alighierir@uanl.edu.mx>
2025-08-08 21:13:52 -0600

    Diasable luarocks as is unused. Set install window colorscheme to gruvbox

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -28,8 +28,12 @@ require("lazy").setup({
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
+  -- Disable rocks support, as it's not needed
+  rocks = {
+    enabled = false
+  },
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "gruvbox", "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
